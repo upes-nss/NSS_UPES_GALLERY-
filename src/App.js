3fac513548fd80bcc4460afc805b1ad2e93288c6
Your Name <500087857@stu.upes.ac.in>
2022-10-29 23:34:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+// import './App.css';
+// import nssgallery from './nssgallery';
 
-function App() {
+// function App() {
+//   // const[image]=useState(image.nssgallery);
+//   return (
+//     <div>
+//       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+//         <div className="container-fluid">
+//           <a className="navbar-brand" href="#">
+//             <img src='https://www.freelogovectors.net/wp-content/uploads/2020/02/nss-logo-national-service-scheme.png' height={40}>
+//             </img> NSS UPES GALLERY
+//           </a>
+//           <button
+//             className="navbar-toggler"
+//             type="button"
+//             data-bs-toggle="collapse"
+//             data-bs-target="#navbarNavAltMarkup"
+//             aria-controls="navbarNavAltMarkup"
+//             aria-expanded="false"
+//             aria-label="Toggle navigation"
+//           >
+//             <span className="navbar-toggler-icon" />
+//           </button>
+
+//           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+//             <div className="navbar-nav">
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 ALL
+//               </a>
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 Cleanliness Drive
+//               </a>
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 Plantation Drive
+//               </a>
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 Donation Drive
+//               </a>
+//               <a className="nav-link active" aria-current="page" href="#">
+//                 Cultural Event
+//               </a>
+//             </div>
+//           </div>
+//         </div>
+//       </nav>
+//       <img className="banner" src={"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695867/samples/tirangarally/DSC_0140-min_d5hp2m.jpg"} alt="BAenner" />
+//       <h1 className='title'> NSS UPES HAPPY MOMENTS </h1>
+//       <div className='image-grid'>
+//         {nssgallery.map((data) =><img src={data.imageurl} alt="BAenner"></img>)}
+
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App
+
+import React,{useState} from 'react';
+import './App.css';
+import Data from './nssgallery';
+const App = () => {
+  const[items,setItems]=useState(Data);
+  const filterItem=(categItem)=>{
+
+    const updatedItems=Data.filter((curele)=>
+    {
+      return curele.category ==categItem;
+    });
+setItems(updatedItems);
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <>
+   
+   
+    <h2 className='mt-5 text-center main-heading'>NSS UPES GALLERY</h2>
+    {/* <img src='https://www.freelogovectors.net/wp-content/uploads/2020/02/nss-logo-national-service-scheme.png' height={70} className='logo'></img> */}
+    <div className="menu-tabs container">
+   <div className='menu-tab d-flex justify-content-around'>
+    <button className='btn btn-warning'onClick={()=>setItems(Data)}>ALL</button>
+    <button className=' btn btn-success'onClick={()=>filterItem('Plantation Drive')}>Plantation Drive</button>
+    <button className='btn btn-warning'onClick={()=>filterItem('Cleanliness Drive')}>Cleanliness Drive</button>
+    <button className='btn btn-warning'onClick={()=>filterItem('Donation Drive')}>Donation Drive</button>
+    <button className='btn btn-success' onClick={()=>filterItem('Cultural Event')}>Cultural Events</button>
+
+
+   </div>
     </div>
-  );
+
+<div className='menu-items contaier-fluid mt-5'>
+  <div className='row'>
+    <div className='col -11 mx-auto'>
+      <div className='row my-5'>
+        {
+          items.map((elem)=>{
+
+            const{id,imageurl,category,des}=elem;
+            return(
+              <div className='item1 col-12 col-md-6 col-lg-6 col-xl-4 my-5'>
+          <div className='row Item-inside'>
+            {/*images*/}
+            <div className='col-12 col-md-12 col-lg-4 img-div'>
+
+              <img src={imageurl} alt='pics' className='img-fluid'/>
+
+            </div>
+            <div className='col-12 col-md-12 col-lg-8'>
+              <div className='main-title pt-4 p-3'>
+                <h2>{category}</h2>
+                <p>{des}</p>
+              </div>
+              <div className='date-time'>
+                <div className='about-image'>
+                  <h5>12/2/2024</h5>
+                </div>
+              </div>
+            </div>
+            </div>
+</div>
+            )
+
+          })
+        }
+        </div>
+          </div>
+         </div>
+      </div>
+  </>
+)
 }
 
-export default App;
+export default App
+
+
---
+++ src/nssgallery.js
@@ -0,0 +1,278 @@
+const Data=[
+    {
+        id:1,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666694487/samples/plantationdrive/DSC_0168_kp39ao.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:2,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666694484/samples/plantationdrive/IMG_20220810_154921_yxmfbm.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:3,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666694483/samples/plantationdrive/IMG_20220810_154632_glb3ri.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:4,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666694482/samples/plantationdrive/DSC_0158_l6xhpc.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:5,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666694469/samples/plantationdrive/IMG20220810155306_cjwuwy.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:6,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667034258/samples/plantationdrive/DSC_0168_zhzhdo.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:7,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667034604/samples/plantationdrive/plantation_lsqp03.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:8,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667034779/samples/plantationdrive/DSC_0148_twodhf.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:9,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667034916/samples/plantationdrive/IMG20220810155514_nrii8s.jpg",
+        category:"Plantation Drive",
+        des:'xyxzz',
+
+    },
+    {
+        id:10,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666683609/samples/snehutsav/IMG_4555_l7uhse.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:11,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666698053/samples/24sepdrive/DSC_0958-min_ezilkc.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:12,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666697955/samples/24sepdrive/DSC_0052_1_-min_1_wupnem.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:13,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666697954/samples/24sepdrive/DSC_0943-min_1_uuy6jy.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:14,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666698053/samples/24sepdrive/DSC_0018_1_-min_mk9clf.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:15,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666684026/samples/snehutsav/DSC_0041_2_hugisp.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:16,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666684027/samples/snehutsav/DSC_0056_2_ireyhf.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:17,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666685799/samples/snehutsav/DSC_0031-min_gbav21.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:18,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666684028/samples/snehutsav/DSC_0044_2_sfoaxo.jpg",
+        category:"Cleanliness Drive",
+        des:'yyayyyaa'
+    },
+    {
+        id:19,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666686765/samples/Sehutsav-20sep-DD%20/DSC_0218_egwda7.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:20,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666686765/samples/Sehutsav-20sep-DD%20/DSC_0186_1_w9mghr.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:21,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667061850/samples/Sehutsav-20sep-DD%20/IMG20220921110350_fllb5z.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:22,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667061845/samples/Sehutsav-20sep-DD%20/DSC_0181_ow7l1s.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:23,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667061971/samples/Sehutsav-20sep-DD%20/DSC_0213-min_oxdg4p.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    }, {
+        id:24,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666687005/samples/Sehutsav-20sep-DD%20/20220921_115122AMByGPSMapCamera_hi2qot.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:25,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667063713/samples/Blood%20Donation%20/blood_donation_gz80hb.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    }, {
+        id:26,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667063742/samples/Blood%20Donation%20/WhatsApp_Image_2022-10-29_at_10.42.17_PM_tdcyet.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:27,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667063709/samples/Blood%20Donation%20/IMG-20220424-WA0019_mzpoad.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:28,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667063710/samples/Blood%20Donation%20/IMG-20220424-WA0025_rvkko7.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:29,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667063744/samples/Blood%20Donation%20/WhatsApp_Image_2022-10-29_at_10.44.05_PM_mhhs0w.jpg",
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:30,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667064162/samples/Blood%20Donation%20/IMG-20220424-WA0033_bqkntz.jpg",
+     
+        category:"Donation Drive",
+        des:'ssssssssssssss'
+    },
+    {
+        id:31,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667064161/samples/Blood%20Donation%20/IMG-20220424-WA0028_lvfc5w.jpg",
+        category:"Donation Drive",
+        des:'sssssssssssss'
+    },
+    {
+        id:32,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695867/samples/tirangarally/DSC_0140-min_d5hp2m.jpg",
+        category:"Cultural Event",
+        des:'eveeeeeve'
+    },
+  
+    {
+        id:34,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695865/samples/tirangarally/DSC_0129-min_kysze3.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:35,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695863/samples/tirangarally/DSC_1001-min_bvoofu.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:36,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695861/samples/tirangarally/DSC_0079-min_ymri7x.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:37,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695860/samples/tirangarally/DSC_0052-min_xxjwrz.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:38,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1666695859/samples/tirangarally/DSC_0076-min_bwdfzn.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:39,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065802/samples/day24/DSC_0167-min_1_h6yc57.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:40,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065802/samples/day24/IMG_4109-min_iipsyz.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+   
+    {
+        id:42,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065802/samples/day24/DSC_0033-min_1_omeaus.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:43,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065802/samples/day24/DSC_0198-min_lkzcng.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:44,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065802/samples/day24/DSC_0167-min_1_h6yc57.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:44,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065801/samples/day24/DSC_0103-min_goqymh.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+    {
+        id:45,
+        imageurl:"https://res.cloudinary.com/dbfyrrym6/image/upload/v1667065801/samples/day24/IMG_4039-min_1_iiu1jq.jpg",
+        category:"Cultural Event",
+        des:'eveeeee'
+    },
+ 
+];
+export default Data;
